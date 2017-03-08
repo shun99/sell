67c118d2d903a6718b763804d736510ceafee53f
chang shunshun <yghysdr@163.com>
2017-03-08 20:12:47 +0800

    完成Header.vue的基本布局

--- README.md
+++ README.md
@@ -64,3 +64,22 @@ resolve: {
 # vue-resource
 # 父组件向子组件传递数据
 App.vue向Header.vue传递数据。
+# Header.vue
+### 根据屏幕大小自动选择图片大小
+```
+bg-image($url)
+  background-image: url($url + "@2x.png")
+  @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-ratio: 3)
+    background-image: url($url + "@3x.png")
+```
+### span
+是行内元素，他的大小必须通过内容来撑开，设置宽高没用，通常将其 display改成 inline-block，这样不需要内容撑开，只需要设置宽高
+### 超出一行展示
+CSS：需要加上宽度(width:100px)、超出隐藏（overflow:hidden;）、强制在同一行显示（white-space: nowrap;）、省略号（text-overflow:ellipsis;）
+```
+p{width: 100px; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;}
+```
+### position:absolute
+absolute 是最棘手的position值。 absolute 与 fixed 的表现类似，但是它不是相对于视窗而是相对于最近的“positioned”祖先元素。如果绝对定位（position属性的值为absolute）的元素没有“positioned”祖先元素，那么它是相对于文档的 body 元素，并且它会随着页面滚动而移动。记住一个“positioned”元素是指 position 值不是 static 的元素
+元素框从文档流完全删除，并相对于其包含块定位。包含块可能是文档中的另一个元素或者是初始包含块。元素原先在正常文档流中所占的空间会关闭，就好像元素原来不存在一样。元素定位后生成一个块级框，而不论原来它在正常流中生成何种类型的框。
+***不要轻易设置absolute父元素的宽度，否者，当父元素内容充满，该元素会被挤出视图内，虽然absolute元素脱离了父元素的文档流，如果不设置宽度则不会被挤出*
--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,17 +1,157 @@
 <template>
   <div class="header">
-    <div class="content-wrapper">{{seller}}</div>
-    <div class="bulletin-wrapper">内容</div>
-    <div class="background"></div>
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img :src="seller.avatar" width="64" height="64">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default{
-    props: ['seller']
+    props: ['seller'],
+    created () {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    }
   };
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .header
+    color: #ffffff
+    position: relative
+    background: rgba(7, 17, 27, 0.5)
+    .content-wrapper
+      position: relative
+      padding: 24px 12px 16px 24px
+      font-size: 0px
+      .avatar
+        display: inline-block
+        img
+          border-radius: 2px
+      .content
+        margin-left: 16px
+        margin-top: 2px
+        vertical-align: top
+        display: inline-block
+        .title
+          font-size: 16px
+          .brand
+            vertical-align: top
+            display: inline-block
+            width: 30px
+            height: 18px
+            bg-image('brand')
+            background-repeat: no-repeat
+            background-size: 30px 18px
+        .description
+          margin-top: 8px
+          font-size: 12px
+        .support
+          margin-top: 10px
+          .icon
+            vertical-align: top
+            display: inline-block
+            width: 12px
+            height: 12px
+            background-repeat: no-repeat
+            background-size 12px 12px
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            margin-left: 4px
+            font-size: 10px
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding-left: 8px
+        padding-right: 8px
+        height: 24px
+        line-height: 24px
+        text-align: center
+        background: rgba(0, 0, 0, 0.2)
+        border-radius: 14px
+        .count
+          font-size: 10px
+          line-height: 24px
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          line-height: 24px
+          font-size: 10px
+          text-align: center
+    .bulletin-wrapper
+      background: rgba(7, 17, 27, 0.2)
+      padding-left: 12px
+      padding-right: 12px
+      height: 28px
+      line-height: 28px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      .bulletin-title
+        display: inline-block
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        margin-top: 8px
+        background-size: 22px 12px
+        background-repeat: no-repeat
+        vertical-align: top
+      .bulletin-text
+        font-size: 10px
+        vertical-align: top
+        margin-left: 4px
+        margin-right: 4px
+      .icon-keyboard_arrow_right
+        position: absolute
+        right: 0px
+        font-size: 10px
+        vertical-align: top
+        margin-top: 9px
+    .background
+      position: absolute
+      top: 0px
+      left: 0px
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 
 </style>
